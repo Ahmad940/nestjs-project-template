51e9d14f5aa12d8f77d0b42b7e8a6c80723eff0d
ahmad940 <ahmadmuhammadmak5@gmail.com>
2021-05-02 04:10:52 +0100

    Added fastify

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import * as helmet from 'helmet';
-import * as compression from 'compression';
+import compression from 'fastify-compress';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import helmet from 'fastify-helmet';
 
 declare const module: any;
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
 
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.register(compression);
+  await app.register(helmet);
 
   app.enableCors();
   app.setGlobalPrefix('api');
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
-  app.use(helmet());
-  app.use(compression());
 
   await app.listen(port);
 
